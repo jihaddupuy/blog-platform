690e1fd47ea4af3b75272401568ed697105fc7ca
jihaddupuy <jihad.dupuy@gmail.com>
2020-10-22 20:34:17 -0400

    category tested and funtional

--- BlogPlatform/Models/Category.cs
+++ BlogPlatform/Models/Category.cs
@@ -7,6 +7,15 @@ namespace blog_template_practice.Models
 {
     public class Category
     {
+        public int Id {get; set;}
+        public string Name { get; set; }
+        public string Content { get; set; }
 
+        public Category(int id, string name, string content)
+        {
+            Id = id;
+            Name = name;
+            Content = content;
+        }
     }
 }
