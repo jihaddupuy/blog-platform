9cc881002027332b5bf0f29514d0560d161a3dde
jihaddupuy <jihad.dupuy@gmail.com>
2020-10-22 21:22:36 -0400

    content tested and funtional

--- BlogPlatform/Models/Content.cs
+++ BlogPlatform/Models/Content.cs
@@ -7,21 +7,22 @@ namespace blog_template_practice.Models
 {
     public class Content
     {
+        public int Id { get; set; }
         public string Title { get; set; }
         public string Body { get; set; }
         public string Author { get; set; }
         public string PublishDate { get; set; }
-        public int Id { get; set; }
+
 
         //public string category()
 
-        public Content(string title, string body, string author, string publishDate, int id)
+        public Content(int id, string title, string body, string author, string publishDate)
         {
+            Id = id;
             Title = title;
             Body = body;
             Author = author;
             PublishDate = publishDate;
-            Id = id;
         }
 
     }
